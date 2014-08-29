2d6e5bc3bc4cb451cb2fe96d2c9c14261e325ecf
programmer-rkt <rajeevphpdeveloper@gmail.com>
2014-08-29 21:23:53 +0530

    initial commit

---
+++ app/code/community/Programmerrkt/Layerednav/Model/Layer/Filter/Category.php
@@ -0,0 +1,44 @@
+<?php
+
+class Programmerrkt_Layerednav_Model_Layer_Filter_Category extends Mage_Catalog_Model_Layer_Filter_Category {
+	/**
+     * Get data array for building category filter items
+     *
+     * @return array
+     */
+    protected function _getItemsData()
+    {
+        $key = $this->getLayer()->getStateKey().'_SUBCATEGORIES';
+        $data = $this->getLayer()->getAggregator()->getCacheData($key);
+
+        if ($data === null) {
+            $categoty   = $this->getCategory();
+            /** @var $categoty Mage_Catalog_Model_Categeory */
+            $categories = $categoty->getChildrenCategories();
+
+            $this->getLayer()->getProductCollection()
+                ->addCountToCategories($categories);
+
+            $data = array();
+
+            foreach ($categories as $category) {
+                if ($category->getIsActive() && $category->getProductCount()) {
+                    $data[] = array(
+                        'label' => Mage::helper('core')->escapeHtml($category->getName()),
+                        'value' => $category->getId(),
+                        'count' => $category->getProductCount(),
+                    );
+                }
+            }
+            
+           	function cmp($a, $b)
+			{
+				return strcmp($a["label"], $b["label"]);
+			}
+			usort($data, "cmp");
+            $tags = $this->getLayer()->getStateTags();
+            $this->getLayer()->getAggregator()->saveCacheData($data, $key, $tags);
+        }
+        return $data;
+    }
+}
